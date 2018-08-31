28d7c68a70eed830d538e973a3b839b20fa67f5b
Patil, Saurabh <saurabhpatil6289@gmail.com>
2018-08-31 14:02:18 -0400

    Configuring urls for displaying group tables

--- aws_connect/views.py
+++ aws_connect/views.py
@@ -6,6 +6,6 @@ def index(request):
     super_groups = SuperGroup.objects.values('super_group').distinct()
     return render(request, 'base.html', {'super_groups': super_groups})
 
-def get_cpt_groups(request):
-    cpt_groups = SuperGroup.objects.filter()
+def get_cpt_groups(request, super_group):
+    cpt_groups = SuperGroup.objects.filter(super_group=super_group)
     return render(request, 'cpt_groups.html', {'groups': cpt_groups})
--- aws_poc/urls.py
+++ aws_poc/urls.py
@@ -20,5 +20,5 @@ import aws_connect.views as views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.index),
-    path('cpt-groups', views.get_cpt_groups, name="cpt_groups")
+    path('api/cpt-groups/<super_group>', views.get_cpt_groups, name="cpt_groups")
 ]
